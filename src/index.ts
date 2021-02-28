42177cee622b9b28209106aad880349e25a3bbbf
DomDomo <g.domis02@gmail.com>
2021-03-01 00:06:05 +0200

    Setup Apollo server with express

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,28 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from 'express';
+import { ApolloServer} from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
+  // const post = orm.em.create(Post, {title: "My first post"});
+  // await orm.em.persistAndFlush(post);
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  })
+
+  apolloServer.applyMiddleware({app});
 
   app.get("/", (_, res) => {
     res.send("hello");
@@ -20,8 +34,6 @@ const main = async () => {
   app.listen(4000, () => {
     console.log("Server runnig on localhost:4000");
   })
-
-
 };
 
 main().catch((err) => {
